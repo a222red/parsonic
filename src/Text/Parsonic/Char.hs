4f4355a505df60fb88a87b4408c04b15498b5a30
a222red <73256177+a222red@users.noreply.github.com>
2022-07-16 09:34:53 -0500

    Add `spaced` and `maybeSpaced` combinators

--- src/Text/Parsonic/Char.hs
+++ src/Text/Parsonic/Char.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE Safe #-}
 
 module Text.Parsonic.Char (
-    alpha, digit, hexDigit, alphaNum, space
+    alpha, digit, hexDigit, alphaNum, space, spaced, maybeSpaced
 ) where
 
-import Text.Parsonic (Parser, satisfy)
-import safe Data.Char(isAlpha, isAlphaNum, isDigit, isHexDigit, isSpace)
+import Text.Parsonic (Parser, satisfy, many, some, interspersedWith)
+import safe Data.Char (isAlpha, isAlphaNum, isDigit, isHexDigit, isSpace)
 
 alpha :: Parser Char e Char
 alpha = satisfy isAlpha
@@ -21,3 +21,9 @@ alphaNum = satisfy isAlphaNum
 
 space :: Parser Char e Char
 space = satisfy isSpace
+
+spaced :: [Parser Char e Char] -> Parser Char e String
+spaced = interspersedWith (some space)
+
+maybeSpaced :: [Parser Char e Char] -> Parser Char e String
+maybeSpaced = interspersedWith (many space)
